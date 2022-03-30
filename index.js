8b6be3c5e034e11c5b2c64442616d47b45fbf07f
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-03-30 15:52:30 -0400

    added get routes and served documentation file with express.static

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Tesing to see if it works.');
+});
+
+app.use(express.static('public'));
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
