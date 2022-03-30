f6898c3cbc232e1421f67754a533170d29046b0a
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-03-30 16:24:41 -0400

    added morgan to log requests and created error handling function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan');
+
 const app = express();
+
+
+
+app.use(morgan('common'));
+
+app.use(express.static('public'));
 
 app.get('/movies', (req, res) => {
   res.json(topMovies);
@@ -9,7 +17,10 @@ app.get('/', (req, res) => {
   res.send('Tesing to see if it works.');
 });
 
-app.use(express.static('public'));
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Something broke!');
+// });
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
