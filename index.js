deb59788a68325d7705213327220e4d82d9051a6
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-05-06 13:55:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -298,3 +298,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
+});
