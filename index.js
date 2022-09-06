8a793f32283f45e6212bc0f371152d6a0d72333a
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-09-06 01:15:07 -0400

    allowed netlify site

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234', 'http://localhost:4200', 'https://guttmanmyflix.netlify.app/'];
+let allowedOrigins = ['http://localhost:1234', 'http://localhost:4200', 'https://adamsmyflix.netlify.app/'];
 
 app.use(cors({
   origin: (origin, callback) => {
