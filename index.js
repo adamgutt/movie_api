eca86225af44b12b8b4f444b93b7d48824ae5156
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-09-13 01:06:58 -0400

    cors error

--- index.js
+++ index.js
@@ -36,6 +36,16 @@ app.use(cors({
   }
 }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 let auth = require('./auth')(app);
 
 const passport = require('passport');
