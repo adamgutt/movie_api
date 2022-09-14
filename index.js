cc986230d62eb708168b1dab1a4f6616550767bc
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-09-14 14:52:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,16 +25,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 let allowedOrigins = ['http://localhost:1234', 'http://localhost:4200', 'https://adamsmyflix.netlify.app, https://willowy-sorbet-614005.netlify.app', "https://631fd4b9cf967200098320ef--adamsmyflix.netlify.app"];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+
+app.use(cors())
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
